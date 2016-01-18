45bd89637bf7d4e33af4adc76dc7e4c91a257232
Erickson Juang <a820224@gmail.com>
2016-01-19 03:20:20 +0800

    get search result

---
+++ crawler.py
@@ -0,0 +1,70 @@
+# coding=UTF-8
+import requests
+from bs4 import BeautifulSoup
+
+URL_HOME = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY01_1.aspx"
+URL_SEARCH = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx"
+DATA = {
+    'v_court': "TPD 臺灣臺北地方法院",
+    'v_sys': "V",
+    'jud_year': "",
+    'sel_judword': "常用字別",
+    'jud_case': "",
+    'jud_no': "",
+    'jud_no_end': "",
+    'jt': "",
+    'dy1': "95",
+    'dm1': "1",
+    'dd1': "1",
+    'dy2': "98",
+    'dm2': "9",
+    'dd2': "21",
+    'jmain1': "",
+    'kw': "業務過失",
+    'keyword': "業務過失",
+    'sdate': "20060101",
+    'edate': "20090921",
+    'jud_title': "",
+    'jmain': "",
+    'Button': " 查詢",
+    'searchkw': "過失"
+}
+
+headers = {
+    "Host": " jirs.judicial.gov.tw",
+    "Connection": " keep-alive",
+    "Content-Length": " 438",
+    "Cache-Control": " max-age=0",
+    "Accept": " text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Origin": " http://jirs.judicial.gov.tw",
+    "Upgrade-Insecure-Requests": " 1",
+    "User-Agent": " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    "Content-Type": " application/x-www-form-urlencoded",
+    "DNT": " 1",
+    "Referer": " http://jirs.judicial.gov.tw/FJUD/FJUDQRY01_1.aspx",
+    "Accept-Encoding": " gzip, deflate",
+    "Accept-Language": " zh-TW,zh;q=0.8,ja;q=0.6,en;q=0.4"
+}
+
+request = requests.Session()
+
+content = request.post(URL_SEARCH, data=DATA, headers=headers).content
+
+dom = BeautifulSoup(content, "html.parser")
+
+try:
+    for row in dom.find('table', id='Table3').find_all('tr'):
+        fields = row.find_all('td')
+        if len(fields) > 0:
+            entity = {
+                'SN': fields[0].text,
+                'judgment_number': fields[1].text,
+                'judgement_link': fields[1].find('a').href,
+                'date': fields[2].text,
+                'summary': fields[3].text
+            }
+            print entity
+except Exception, e:
+    print '===============html content===============\n', content
+    print '===============DOM===============\n', dom
+    print '===============error===============\n', e
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Requirement
+Python 2.7
+### Python package
++ requests
++ BeautifulSoup
